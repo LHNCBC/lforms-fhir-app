b0f6bb1242e27d940ba8e8183a73ee7f2046e7ec
Paul Lynch <plynch@mail.nih.gov>
2019-05-23 18:15:29 -0400

    Fixed addition of link on Questionnaire ID so it does not change the ID value

--- app/fhir-app/fhir-app-content.controller.js
+++ app/fhir-app/fhir-app-content.controller.js
@@ -266,8 +266,8 @@ angular.module('lformsApp')
           if (q) {
             var fhirString = JSON.stringify(q, null, 2);
             var serverBaseURL = fhirService.getServerBaseURL();
-            fhirString = fhirString.replace(/"id": "(\d+)"/, '"id": "<a href="'+
-              serverBaseURL+'/Questionnaire/$1" target=_blank>Questionnaire/$1</a>');
+            fhirString = fhirString.replace(/"id": "([^\s"]+)"/, '"id": "<a href="'+
+              serverBaseURL+'/Questionnaire/$1" target=_blank>$1</a>');
             $scope.fhirResourceString = fhirString;
             $scope.fhirResourceTitle = "Questionnaire Resource from FHIR Server";
 
