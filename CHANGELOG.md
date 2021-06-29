b0a49408774c5e37e93c7c0980c1c5969a0b846b
Wang, Ye (NIH/NLM/LHC) [E] <wangye@nih.gov>
2021-06-29 11:03:46 -0400

    add the missing icon in the production mode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 This log documents the significant changes for each release.
 This project follows [Semantic Versioning](http://semver.org/).
 
+## [1.3.1] - 2021-06-29
+### Fixed
+- Added a missing icon in production mode.
+
 ## [1.3.0] - 2021-05-13
 ### Added
 - Added the display of the pratitioner's name on the page
--- Gruntfile.js
+++ Gruntfile.js
@@ -216,7 +216,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             'bower_components/**/**',
-            'assets/images/{,*/}*.{jpg,gif,svg}',
+            'assets/images/{,*/}*.{jpg,gif,svg,png}',
             'assets/fonts/**/*',
             'launch.html',
             'index.html',
